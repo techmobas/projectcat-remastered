569aba8cffb95d01296829e098e2d790d2846661
techmobas <techmobabas@gmail.com>
2022-07-26 06:35:50 +0700

    disable 2 touch function

--- Assets/Script/ButtonFunction.cs
+++ Assets/Script/ButtonFunction.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
 
-public class ButtonFunction : MonoBehaviour
-{
-    private InputManager inputManager;
+// public class ButtonFunction : MonoBehaviour
+// {
+//     private InputManager inputManager;
 
-    private void Awake(){
-        inputManager = InputManager.Instance;
-    }
+//     private void Awake(){
+//         inputManager = InputManager.Instance;
+//     }
 
-    private void OnEnable(){
-        inputManager.OnStartTouch += ReTouch;
-    }
+//     private void OnEnable(){
+//         inputManager.OnStartTouch += ReTouch;
+//     }
 
-    private void OnDisable(){
-        inputManager.OnEndTouch -= ReTouch;
-    }
+//     private void OnDisable(){
+//         inputManager.OnEndTouch -= ReTouch;
+//     }
 
-    public void ReTouch(Vector2 screenPosition, float time){
-        Vector3 screenCoordinates = new Vector3(screenPosition.x, screenPosition.y, Camera.main.nearClipPlane);
-        Vector3 worldCoordinates = Camera.main.ScreenToWorldPoint(screenPosition);
-        worldCoordinates.z = 0;
-    }
-}
+//     public void ReTouch(Vector2 screenPosition, float time){
+//         Vector3 screenCoordinates = new Vector3(screenPosition.x, screenPosition.y, Camera.main.nearClipPlane);
+//         Vector3 worldCoordinates = Camera.main.ScreenToWorldPoint(screenPosition);
+//         worldCoordinates.z = 0;
+//     }
+// }
